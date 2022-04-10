734cea3cd04626c4c22db277ca26bb324d574b97
hwangdayoung <s2018s39@e-mirim.hs.kr>
2022-04-10 17:32:34 +0900

    색상 테마 추가

---
+++ assets/theme/colors.js
@@ -0,0 +1,44 @@
+const Colors = {
+  base_black: '#000000',
+  base_white: '#FFFFFF',
+
+  system_grey_1: '#8E8E93',
+  system_grey_2: '#AEAEB2',
+  system_grey_3: '#C7C7CC',
+  system_grey_4: '#D1D1D6',
+  system_grey_5: '#E5E5EA',
+  system_grey_6: '#F2F2F7',
+
+  system_tint_blue: '#007AFF',
+  system_tint_green: '#34C759',
+  system_tint_indigo: '#5856D6',
+  system_tint_orange: '#FF9500',
+  system_tint_pink: '#FF2D55',
+  system_tint_purple: '#AF52DE',
+  system_tint_red: '#FF3B30',
+  system_tint_teal: '#5AC8FA',
+  system_tint_yellow: '#FFCC00',
+
+  system_bg_primary: '#FFFFFF',
+  system_bg_secondary: '#F2F2F7',
+  system_bg_tertiary: '#FAFAFA',
+
+  text_primary: '#0F0F10',
+  text_secondary: '#8A8A8E',
+  text_tertiary: '#ADADAD',
+  text_quarternary: '#DCDCDD',
+
+  green_1000: '#007B00',
+  green_900: '#019400',
+  green_800: '#01AD00',
+  green_700: '#01D900',
+  green_600: '#01D700',
+  green_500: '#01F700',
+  green_400: '#2FFF2E',
+  green_300: '#59FF58',
+  green_200: '#80FF80',
+  green_100: '#A5FFA5',
+  green_50: '#BBFFBB',
+};
+
+export default Colors;
