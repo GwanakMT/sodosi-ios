f9da0ab4b17e028e24cbfdbf2d115a3a0ad531c6
hwangdayoung <s2018s39@e-mirim.hs.kr>
2022-04-10 17:13:00 +0900

    Typography 컴포넌트 추가

---
+++ components/Typography.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Text, StyleSheet} from 'react-native';
+
+function Typography(props) {
+  const {variant, bold, align, color, customStyles, children} = props;
+
+  return (
+    <Text
+      style={[
+        styles.typography,
+        variant === 'hugeTitle1' && styles.hugeTitle1,
+        variant === 'hugeTitle2' && styles.hugeTitle2,
+        variant === 'largeTitle' && styles.largeTitle,
+        variant === 'title1' && styles.title1,
+        variant === 'title2' && styles.title2,
+        variant === 'title3' && styles.title3,
+        variant === 'headline' && styles.headline,
+        variant === 'callout' && styles.callout,
+        variant === 'subheadline' && styles.subheadline,
+        variant === 'body' && styles.body,
+        variant === 'caption' && styles.caption,
+        bold === true &&
+          (variant === 'callout' ||
+          variant === 'subheadline' ||
+          variant === 'body' ||
+          variant === 'caption'
+            ? styles.semiBold
+            : styles.bold),
+        align === 'left' && styles.left,
+        align === 'center' && styles.center,
+        align === 'right' && styles.right,
+        {color: color},
+        customStyles,
+      ]}>
+      {children}
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  typography: {
+    fontFamily: 'Pretendard-Regular',
+  },
+  hugeTitle1: {
+    fontSize: 56,
+    lineHeight: 84,
+    letterSpacing: 0.25,
+  },
+  hugeTitle2: {
+    fontSize: 50,
+    lineHeight: 75,
+    letterSpacing: 0.25,
+  },
+  largeTitle: {
+    fontSize: 34,
+    lineHeight: 42,
+    letterSpacing: 0.374,
+  },
+  title1: {
+    fontSize: 26,
+    lineHeight: 36,
+    letterSpacing: 0.364,
+  },
+  title2: {
+    fontSize: 22,
+    lineHeight: 32,
+  },
+  title3: {
+    fontSize: 20,
+    lineHeight: 24,
+    letterSpacing: 0.38,
+  },
+  headline: {
+    fontSize: 16,
+    lineHeight: 22,
+    letterSpacing: -0.32,
+    fontFamily: 'Pretendard-Bold',
+    // fontFeatureSettings: 'ss03' on,
+  },
+  callout: {
+    fontSize: 16,
+    lineHeight: 22,
+    letterSpacing: -0.32,
+  },
+  subheadline: {
+    fontSize: 15,
+    lineHeight: 20,
+    letterSpacing: -0.24,
+  },
+  body: {
+    fontSize: 14,
+    lineHeight: 22,
+    letterSpacing: -0.408,
+  },
+  caption: {
+    fontSize: 13,
+    lineHeight: 20,
+  },
+  semiBold: {
+    fontFamily: 'Pretendard-SemiBold',
+  },
+  bold: {
+    fontFamily: 'Pretendard-bold',
+  },
+  left: {
+    textAlign: 'left',
+  },
+  center: {
+    textAlign: 'center',
+  },
+  right: {
+    textAlign: 'right',
+  },
+});
+
+Typography.defaultProps = {
+  bold: false,
+  align: 'left',
+};
+
+Typography.propTypes = {
+  variant: PropTypes.string,
+  bold: PropTypes.bool,
+  align: PropTypes.string,
+  color: PropTypes.string,
+  customStyles: PropTypes.object,
+  children: PropTypes.any,
+};
+
+export default Typography;
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CafeGif from '../assets/images/cafe.gif';
 import {StatusBar, View, Image, Text, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Button} from '../components';
+import {Typography, Button} from '../components';
 
 function StartScreen({navigation}) {
   return (
@@ -11,20 +11,29 @@ function StartScreen({navigation}) {
       <View style={styles.container}>
         <View style={styles.content}>
           <Image source={CafeGif} style={styles.gif} />
-          <Text style={styles.title}>공부하기 좋은 카페 어디지?</Text>
-          <Text style={styles.description}>
+          <Typography
+            variant="title2"
+            color="white"
+            bold
+            customStyles={styles.title}>
+            공부하기 좋은 카페 어디지?
+          </Typography>
+          <Typography
+            align="center"
+            color="white"
+            customStyles={styles.description}>
             우리는 모두 다양한 취향을 가지고 있습니다.{'\n'}
             소도시에서 내 취향을 담은 장소를 만나보세요
-          </Text>
+          </Typography>
         </View>
         <View style={styles.buttonWrap}>
           <Button
             type="primary"
-            title="시작하기"
             customStyles={styles.startButton}
-            onPress={() => navigation.navigate('')}
-          />
-          <Button type="outlined" title="이미 소도시 회원이에요!" />
+            onPress={() => navigation.navigate('Phone')}>
+            시작하기
+          </Button>
+          <Button type="outlined">이미 소도시 회원이에요!</Button>
         </View>
       </View>
     </SafeAreaView>
@@ -57,20 +66,13 @@ const styles = StyleSheet.create({
     marginBottom: 32,
   },
   title: {
-    fontFamily: 'Pretendard-Bold',
-    fontSize: 22,
-    color: 'white',
     paddingBottom: 14,
-    lineHeight: 32,
   },
   description: {
-    fontFamily: 'Pretendard-Regular',
     fontSize: 15,
-    color: 'white',
     opacity: 0.7,
     lineHeight: 22,
     letterSpacing: -0.24,
-    textAlign: 'center',
   },
   startButton: {
     marginBottom: 14,
