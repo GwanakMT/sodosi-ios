9f1335e34af4d018f9f70e6b66bcc6338a4a6b3b
hwangdayoung <s2018s39@e-mirim.hs.kr>
2022-04-23 20:47:40 +0900

    screens import 변경

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import StartScreen from './screens/StartScreen';
-import PhoneScreen from './screens/PhoneScreen';
-import CertificationNumberScreen from './screens/CertificationNumberScreen';
-import PasswordScreen from './screens/PasswordScreen';
-import NicknameScreen from './screens/NicknameScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  StartScreen,
+  PhoneScreen,
+  CertificationNumberScreen,
+  PasswordScreen,
+  NicknameScreen,
+  WelcomeScreen,
+} from './screens';
 
 const Stack = createNativeStackNavigator();
 
---
+++ screens/index.js
@@ -0,0 +1,6 @@
+export {default as StartScreen} from './StartScreen';
+export {default as PhoneScreen} from './PhoneScreen';
+export {default as CertificationNumberScreen} from './CertificationNumberScreen';
+export {default as PasswordScreen} from './PasswordScreen';
+export {default as NicknameScreen} from './NicknameScreen';
+export {default as WelcomeScreen} from './WelcomeScreen';
